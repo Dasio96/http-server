050c16e7c337442ac08a37dabfb5dd4308a1e2aa
Dasio96 <dawid01852@gmail.com>
2026-08-22 17:58:45 +0200

    feat(server): create and bind TCP socket to port

--- main.c
+++ main.c
@@ -4,10 +4,24 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define PORT 8080
+
 int main(void) {
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
     perror("Socket creation failed");
+    return EXIT_FAILURE;
+  }
+
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_port = htons(PORT);
+  address.sin_addr.s_addr = INADDR_ANY;
+
+  int res = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+  if (res < 0) {
+    perror("Bind failed");
+    close(server_fd);
     return EXIT_FAILURE;
   }
 
