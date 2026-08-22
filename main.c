18c991e42b59c27f9611e35e06e66518ea550116
Dasio96 <dawid01852@gmail.com>
2026-08-23 00:52:32 +0200

    feat(server): accept and register client sockets in epoll

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 
 #define PORT 8080
+#define MAX_EVENTS 10
 
 int set_nonblocking(int fd) {
   int flags = fcntl(fd, F_GETFL, 0);
@@ -64,6 +65,44 @@ int main(void) {
     close(server_fd);
     close(epoll_fd);
     return EXIT_FAILURE;
+  }
+
+  struct epoll_event events[MAX_EVENTS];
+
+  while (1) {
+    int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+    if (nfds < 0) {
+      perror("Eppol_wait failed");
+      break;
+    }
+
+    for (int i = 0; i < nfds; i++) {
+      if (events[i].data.fd == server_fd) {
+        int client_fd = accept(server_fd, NULL, NULL);
+        if (client_fd < 0) {
+          perror("Accept failed");
+          continue;
+        }
+
+        if (set_nonblocking(client_fd) < 0) {
+          perror("Failed to set client socket non-blocking");
+          close(client_fd);
+          continue;
+        }
+
+        struct epoll_event client_ev;
+        client_ev.events = EPOLLIN;
+        client_ev.data.fd = client_fd;
+
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &client_ev) < 0) {
+          perror("Failed to add client socket to epoll");
+          close(client_fd);
+          continue;
+        }
+
+        printf("%d\n", client_fd);
+      }
+    }
   }
 
   close(server_fd);
